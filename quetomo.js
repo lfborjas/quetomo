47057e6b8b714a188eadc55865485b3d3a9900ae
Luis Borjas <luisfborjas@gmail.com>
2015-02-22 10:16:00 -0500

    reorganizing files

--- quetomo.js
+++ quetomo.js
@@ -1,37 +1,3 @@
-Medications = new Mongo.Collection("medications");
-
-if (Meteor.isClient) {
-  Template.landing.events({
-    'click a.add_medication': function(){
-      $("#add_medication").show();
-    } 
-  });
-
-  Template.add_medication.events({
-    'click #cancel_add_medication': function(){
-      $("#add_medication").hide();
-    }
-    ,'submit form': function () {
-      var form = $(event.target); 
-      var normalize = function(word){
-        return word.toLowerCase();
-      }
-
-      var data = {
-        name: form.find("#name").val(), 
-        ingredients: form.find("#ingredients").val().split(/\s*,\s*/).map(normalize), 
-        symptoms: form.find("#symptoms").val().split(/\s*,\s*/).map(normalize), 
-        warnings: form.find("#warnings").val(),
-        createdAt: new Date(),
-        positive_votes: 0,
-        negative_votes: 0
-      };
-      Medications.insert(data);
-      $("#add_medication").hide();
-    }
-  });
-}
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
