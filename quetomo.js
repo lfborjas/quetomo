04d6004a82b12f071158400c27a0e858610f025c
Luis Borjas <luisfborjas@gmail.com>
2015-02-22 09:38:46 -0500

    form to add new medications

--- quetomo.js
+++ quetomo.js
@@ -1,17 +1,21 @@
+Medications = new Mongo.Collection("medications");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Template.add_medication.events({
+    'submit form': function () {
+      var form = $(event.target); 
+      var normalize = function(word){
+        return word.toLowerCase();
+      }
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      var data = {
+        name: form.find("#name").val(), 
+        ingredients: form.find("#ingredients").val().split(/\s*,\s*/).map(normalize), 
+        symptoms: form.find("#symptoms").val().split(/\s*,\s*/).map(normalize), 
+        warnings: form.find("#warnings").val(),
+        createdAt: new Date(),
+      };
+      Medications.insert(data);
     }
   });
 }
